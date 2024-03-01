16408004534ae7fd6e20cf7f9746a42e9bf5a3e9
Abderrahim LAHYANI <aderhm@proton.me>
2024-03-01 17:38:18 +0100

    Create models: BaseModel & Engine

---
+++ models/base_model.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Contains class BaseModel
+"""
+
+import models
+import sqlalchemy
+from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+
+Base = declarative_base()
+
+class BaseModel:
+    """The BaseModel class from which future classes will be derived.
+    """
+    id = Column(String(60), primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
+
+    def __init__(self, *args, **kwargs):
+        """Initializes the BaseModel.
+        """
+        if kwargs:
+            for k, v in kwargs.items():
+                if k != "__class__":
+                    setattr(self, k, v)
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
+            if kwargs.get("crarted_at", None) and type(self.craeted_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("update_at", None) and type(self.created_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.utcnow()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
+
+    def __str__(self):
+        """Represents the BaseModel class as a string.
+        """
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Saves the last update time
+        """
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
+
+    def delete(self):
+        """Deletes the current instance from the storage.
+        """
+        models.storage.delete(self)
---
+++ models/engine/db_storage.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""Contains the class DBStorage
+"""
+
+import models
+import sqlalchemy
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+classes = {
+            "expense": Expense,
+            "goal": Goal,
+            "income": Income,
+            "user": User
+        }
+
+class DBStorage:
+    """Interacts with the MySQL db.
+    """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Initializes a DBStorage object.
+        """
+        ETA_DB_USER     = getenv('ETA_DB_USER')
+        ETA_DB_PWD      = getenv('ETA_DB_PWD')
+        ETA_DB_HOST     = getenv('ETA_DB_HOST')
+        ETA_DB_NAME     = getenv('ETA_DB_NAME')
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+                                      format(ETA_DB_USER, ETA_DB_PWD, ETA_DB_HOST, ETA_DB_NAME))
+
+    def all(self, cls=None):
+        """Gets all from db.
+        """
+        _dict = {}
+        for c in classes:
+            if cls is None or cls is classes[c] or cls is c:
+                objects = self.__session.query(classes[c]).all()
+                for o in objects:
+                    key = o.__class__.__name__ + '.' + o.id
+                    _dict[key] = o
+        return _dict
+
+    def new(self, obj):
+        """Adds a new object to the database.
+        """
+        self.__session.add(obj)
+
+    def save(self):
+        """Commits changes of the current database session.
+        """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Deletes an object from the database.
+        """
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Reloads data from the database.
+        """
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        sssn = scoped_session(session_factory)
+        self.__session = sssn
+
+    def close(self):
+        """Remove the private session attribute.
+        """
+        self.__session.remove()
+    def get(self, cls, id):
+        """Retrieve object by id
+        """
+        if cls is not None and id is not None:
+            return self.__session.query(cls).filter(cls.id == id).first()
+        else:
+            return None
